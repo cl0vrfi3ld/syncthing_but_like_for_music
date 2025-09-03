b7544c82f6ed6b1efd83880549838afb8805a5f8
cl0vrfi3ld <47996003+cl0vrfi3ld@users.noreply.github.com>
2025-09-02 21:11:45 -0400

    more todos

--- rust/src/backend/sync.rs
+++ rust/src/backend/sync.rs
@@ -1,1 +1,5 @@
 // sending and recieving of files
+
+/// A struct that stores the necessary handles to our various sync engine functions, along with other helper functions
+#[flutter_rust_bridge::frb(opaque)]
+pub struct SyncController {}
