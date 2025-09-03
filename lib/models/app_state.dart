8a335fba27343e34d223a1c9bdcd794d9d447add
cl0vrfi3ld <47996003+cl0vrfi3ld@users.noreply.github.com>
2025-09-02 21:09:05 -0400

    create initial app state model

---
+++ lib/models/app_state.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+import '../src/rust/backend/net.dart';
+
+class AppState {
+  late NetController netController;
+}
+
+class AppStateModel extends ChangeNotifier {
+  final AppState _appState = AppState();
+
+  NetController get netController => _appState.netController;
+
+  void setNetController(NetController controller) {
+    _appState.netController = controller;
+    notifyListeners();
+  }
+}
